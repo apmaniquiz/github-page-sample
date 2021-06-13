e31827818f07dfb30cae919a85afd71313b6a403
Anthony Maniquiz <a.maniquiz@yahoo.com>
2021-06-14 00:40:47 +0800

    test deploy 02

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          <p>Test ci/cd via github action, test num 01</p>
+          <p>Test ci/cd via github action, test num 02</p>
           <br/>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
